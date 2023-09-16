c9e1fb1f7a217a95acbf63c5a42acc86208ae2f4
byigitelmaci <142091808+byigitelmaci@users.noreply.github.com>
2023-09-16 13:13:30 +0300

    Add files via upload

---
+++ Path_sınıfı_dosya_yol_işlemleri_ders_63/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Path_sınıfı_dosya_yol_işlemleri_ders_63
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //textBox1.Text += Path.GetDirectoryName(@"c:\Data(D:)\Program Files\forestt2.jpg") + Environment.NewLine;
+            //textBox1.Text += Path.GetFileName(@"c:\Data(D:)\Program Files\forestt2.jpg") + Environment.NewLine;
+            //textBox1.Text += Path.GetFileNameWithoutExtension(@"c:\Data(D:)\Program Files\forestt2.jpg") + Environment.NewLine;
+            //textBox1.Text += Path.GetExtension(@"c:\Data(D:)\Program Files\forestt2.jpg") + Environment.NewLine;
+            //textBox1.Text += Path.GetPathRoot(@"c:\Data(D:)\Program Files\forestt2.jpg") + Environment.NewLine;
+            //textBox1.Text += Path.GetFullPath(@"c:\Data(D:)\Program Files\forestt2.jpg")+ Environment.NewLine;
+
+            try
+            {
+                textBox1.Text += Path.GetDirectoryName(@"c:\Data(D:)\Program Files\forestt2.jpg") + Environment.NewLine;
+                textBox1.Text += Path.GetFileName(@"c:\Data(D:)\Program Files\forestt2.jpg") + Environment.NewLine;
+                textBox1.Text += Path.GetFileNameWithoutExtension(@"c:\Data(D:)\Program Files\forestt2.jpg") + Environment.NewLine;
+                textBox1.Text += Path.GetExtension(@"c:\Data(D:)\Program Files\forestt2.jpg") + Environment.NewLine;
+                textBox1.Text += Path.GetPathRoot(@"c:\Data(D:)\Program Files\forestt2.jpg") + Environment.NewLine;
+                textBox1.Text += Path.GetFullPath(@"c:\Data(D:)\Program Files\forestt2.jpg") + Environment.NewLine;
+            }
+            catch (Exception ex)
+            {
+                // Hata mesajını textBox1 içine ekleyin
+                textBox1.Text += "Hata: " + ex.Message + Environment.NewLine;
+            }
+
+            
+        }
+
+       
+    }
+}
---
+++ Personel_Takip_Uygulaması/Form1.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace Personel_Takip_Uygulaması
+{
+    partial class FrmLogin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.btnGiris = new System.Windows.Forms.Button();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.txtUserNo = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.Location = new System.Drawing.Point(352, 260);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(192, 91);
+            this.button1.TabIndex = 11;
+            this.button1.Text = "Kapat";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnGiris
+            // 
+            this.btnGiris.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnGiris.Location = new System.Drawing.Point(99, 260);
+            this.btnGiris.Name = "btnGiris";
+            this.btnGiris.Size = new System.Drawing.Size(192, 91);
+            this.btnGiris.TabIndex = 10;
+            this.btnGiris.Text = "Giriş";
+            this.btnGiris.UseVisualStyleBackColor = true;
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtPassword.Location = new System.Drawing.Point(280, 153);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(205, 30);
+            this.txtPassword.TabIndex = 9;
+            // 
+            // txtUserNo
+            // 
+            this.txtUserNo.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtUserNo.Location = new System.Drawing.Point(280, 71);
+            this.txtUserNo.Name = "txtUserNo";
+            this.txtUserNo.Size = new System.Drawing.Size(205, 30);
+            this.txtUserNo.TabIndex = 8;
+            this.txtUserNo.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtUserNo_KeyPress);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(106, 150);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(134, 31);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Password";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(106, 68);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(107, 31);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "UserNo";
+            // 
+            // FrmLogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnGiris);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.txtUserNo);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "FrmLogin";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Kullanıcı Girişi";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnGiris;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.TextBox txtUserNo;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+    }
+}
