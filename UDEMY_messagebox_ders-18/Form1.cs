a27c25c2428b76aa53560bc5d1948bc615105a57
byigitelmaci <142091808+byigitelmaci@users.noreply.github.com>
2023-09-16 13:21:42 +0300

    Add files via upload

---
+++ UDEMY_messagebox_ders-18/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UDEMY_messagebox_ders_18
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            MessageBox.Show("firt message", "message title", MessageBoxButtons.YesNo, MessageBoxIcon.Error);
+
+            //bişey denedim
+            //label1.BackColor = Color.Red;
+            //label1.ForeColor = Color.White; 
+        }
+    }
+}
