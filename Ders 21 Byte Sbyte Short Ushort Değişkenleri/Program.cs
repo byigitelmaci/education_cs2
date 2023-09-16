694b5f85b9662efa38142f1320cf3cabc6297b7e
byigitelmaci <142091808+byigitelmaci@users.noreply.github.com>
2023-09-16 11:00:46 +0300

    Add files via upload

---
+++ Ders 21 Byte Sbyte Short Ushort Değişkenleri/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ders_21_Byte_Sbyte_Short_Ushort_Değişkenleri
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //byte sayi;
+            //sayi= 255;    // byte 0 ile 255 arasındadır.
+            //Console.Write(sayi);
+            //Console.Read();
+
+            //----------------------------------------------------------------------------------------
+
+            //byte s1, s2;
+            //s1 = 20;
+            //s2 = 40;
+            //Console.WriteLine(s1+s2);
+            //Console.Read();
+
+            //------------------------------------------------------------------------------------------
+
+
+            //sbyte deger;
+            //deger = 5;     sbyte -128 ile 127 değerleri arasındadır...
+            //Console.WriteLine(deger);
+            //Console.ReadLine();
+            //-------------------------------------------------------------------------------------------
+
+            //Console.WriteLine("******doğu anadolu bölgesi hava durumu******");
+            //Console.WriteLine("******1 ocak 2023 hava durumu tahminleri******");
+            //sbyte erzurum, malatya, elazığ;
+            //erzurum = -35;
+            //malatya = 3;
+            //elazığ = -7;
+
+            //Console.WriteLine("erzurum karlı:" + erzurum + " derecedir");
+            //Console.WriteLine("malatya çok bulutlu:" + malatya + " derecedir");
+            //Console.WriteLine("elazığ karla karışık yağmurlu:" + elazığ + " derecedir");
+            //Console.ReadLine();
+
+            //---------------------------------------------------------------------------------------------
+
+            //short s1,s2,s3;
+            //s1 = 32767;     short 32767 ile -32768 değerleri arasındadır...
+            //s2 = -32768;
+            //s3 = 40;
+            //Console.WriteLine(s1+s2+s3);
+            //Console.Read();
+            //---------------------------------------------------------------------------------------------
+
+            //ushort s1,s2;
+            //s1 = 0;
+            //s2 = 65535;
+            //Console.WriteLine(s1 + s2);
+            //Console.Read();
+
+        }
+    }
+}
---
+++ Ders 23 Arka Plan Rengi Değiştirme/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ders_23_Arka_Plan_Rengi_Değiştirme
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.BackgroundColor = ConsoleColor.DarkRed;          //arka plan rengini değiştirme 
+            //Console.ForegroundColor = ConsoleColor.Yellow;          //yazının rengini değiştirme
+            //Console.Title = "18 mart 2016 mart";                   //console sayfasının başlığını değiştirme
+            //Console.Clear();
+            //Console.WriteLine("18 Mart Çanakkale Şehitleri Anma Günü");
+            //Console.ReadLine();
+
+            //---------------------------------------------------------------------------------------------------
+
+            Console.WriteLine("lütfen bi sayı giriniz");
+            ConsoleColor renk = (ConsoleColor)Convert.ToInt16(Console.ReadLine());
+            Console.BackgroundColor=renk;
+            Console.Clear();
+            Console.ReadLine();
+        }
+    }
+}
